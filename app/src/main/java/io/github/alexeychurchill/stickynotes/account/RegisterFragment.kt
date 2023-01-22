68b840c42b2bdf456ef7c2e9a4db66f19e9a8792
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-01-23 01:43:44 +0200

    Rewrite register screen in compose

--- app/src/main/java/io/github/alexeychurchill/stickynotes/account/RegisterFragment.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/account/RegisterFragment.kt
@@ -4,73 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
-import androidx.core.view.isInvisible
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.alexeychurchill.stickynotes.R
+import io.github.alexeychurchill.stickynotes.account.ui.RegisterScreen
+import io.github.alexeychurchill.stickynotes.core.StickyNotesTheme
 
 /**
  * Register fragment
  */
 @AndroidEntryPoint
 class RegisterFragment : Fragment() {
-    private var mETLogin: EditText? = null
-    private var mETPassword: EditText? = null
-    private var mBtnRegister: Button? = null
-    private var mPBWait: ProgressBar? = null
-
     private val viewModel: RegisterViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            viewModel.error.collect {
+                Toast.makeText(requireContext(), it, LENGTH_LONG).show()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(
-        R.layout.fragment_register,
-        container,
-        false
-    )
+    ): View = ComposeView(requireContext())
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mETLogin = view.findViewById<View>(R.id.etLogin) as EditText
-        mETPassword = view.findViewById<View>(R.id.etPassword) as EditText
-        mPBWait = view.findViewById<View>(R.id.pbWait) as ProgressBar
-        mBtnRegister = view.findViewById<View>(R.id.btnRegister) as Button
-        initRegisterButtonListener()
-        initProgress()
-        initError()
-    }
-
-    private fun initRegisterButtonListener() {
-        mBtnRegister!!.setOnClickListener {
-            val login = mETLogin!!.text.toString()
-            val password = mETPassword!!.text.toString()
-            viewModel.register(login, password)
-        }
-    }
-
-    private fun initProgress() {
-        lifecycleScope.launchWhenStarted {
-            viewModel.isInProgress.collect { isInProgress ->
-                mPBWait!!.isInvisible = !isInProgress
-                mBtnRegister!!.isInvisible = isInProgress
-            }
-        }
-    }
-
-    private fun initError() {
-        lifecycleScope.launchWhenStarted {
-            viewModel.error.collect { errorResId ->
-                Toast.makeText(requireContext(), errorResId, LENGTH_LONG)
-                    .show()
+        (view as? ComposeView)?.setContent {
+            StickyNotesTheme {
+                RegisterScreen(viewModel)
             }
         }
     }
