9a39dd45e75d2d8b8c184078454a9e2b07848821
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-01-29 03:35:20 +0200

    Remove Firestore annotations

--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/firebase/FirestoreNoteEntry.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/firebase/FirestoreNoteEntry.kt
@@ -1,19 +1,13 @@
 package io.github.alexeychurchill.stickynotes.notes.firebase
 
-import com.google.firebase.firestore.PropertyName
 import io.github.alexeychurchill.stickynotes.core.extension.ofTimeMillis
 import io.github.alexeychurchill.stickynotes.core.model.NoteEntry
 
 data class FirestoreNoteEntry(
-    @PropertyName("owner_id")
     val ownerId: String = "",
-    @PropertyName("title")
     val title: String = "",
-    @PropertyName("subject")
     val subject: String? = null,
-    @PropertyName("created_at")
     val createdAt: Long = 0L,
-    @PropertyName("changed_at")
     val changedAt: Long = 0L,
 ) {
     constructor(noteEntry: NoteEntry): this(
