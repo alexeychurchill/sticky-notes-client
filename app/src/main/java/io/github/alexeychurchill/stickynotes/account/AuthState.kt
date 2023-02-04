5654c81ac9fd62915d00f610c3270a498f634980
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-02-05 00:32:22 +0200

    Remove Account feature and Firebase

--- app/src/main/java/io/github/alexeychurchill/stickynotes/account/AuthState.kt
+++
@@ -1,9 +0,0 @@
-package io.github.alexeychurchill.stickynotes.account
-
-import io.github.alexeychurchill.stickynotes.core.model.User
-
-sealed interface AuthState {
-    object None : AuthState
-
-    data class Authorized(val user: User) : AuthState
-}
--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/domain/NoteEntryFactory.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/domain/NoteEntryFactory.kt
@@ -1,26 +1,16 @@
 package io.github.alexeychurchill.stickynotes.notes.domain
 
-import io.github.alexeychurchill.stickynotes.account.AccountRepository
-import io.github.alexeychurchill.stickynotes.account.AuthState.Authorized
 import io.github.alexeychurchill.stickynotes.core.model.NoteEntry
 import io.github.alexeychurchill.stickynotes.core.model.NoteEntry.Companion.NO_ID
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.first
 import java.util.*
 import javax.inject.Inject
 
-class NoteEntryFactory @Inject constructor(
-    private val accountRepository: AccountRepository,
-) {
+class NoteEntryFactory @Inject constructor() {
     suspend fun create(title: String): NoteEntry {
-        val user = accountRepository.authState
-            .filterIsInstance<Authorized>()
-            .first()
-            .user
         val now = Calendar.getInstance()
         return NoteEntry(
             id = NO_ID,
-            ownerId = user.id,
+            ownerId = NO_ID,
             title = title,
             subject = null,
             changedAt = now,
