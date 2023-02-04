92fd591d96d51cd8990cc51ddcc2e653ad0ea600
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-02-05 00:12:08 +0200

    Remove contacts feature

--- app/src/main/java/io/github/alexeychurchill/stickynotes/contacts/presentation/ContactScreenModeState.kt
+++
@@ -1,6 +0,0 @@
-package io.github.alexeychurchill.stickynotes.contacts.presentation
-
-data class ContactScreenModeState(
-    val all: List<ContactScreenMode> = emptyList(),
-    val selected: ContactScreenMode? = null,
-)
