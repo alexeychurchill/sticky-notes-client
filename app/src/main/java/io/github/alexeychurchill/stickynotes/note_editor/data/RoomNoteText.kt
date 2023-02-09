865ebb5087c049d09553ea40b03bfdc8098944e7
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-02-09 22:13:29 +0200

    Set valid DB schema

--- app/src/main/java/io/github/alexeychurchill/stickynotes/note_editor/data/RoomNoteText.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/note_editor/data/RoomNoteText.kt
@@ -2,15 +2,27 @@ package io.github.alexeychurchill.stickynotes.note_editor.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
+import io.github.alexeychurchill.stickynotes.notes.data.RoomNoteEntry
 
 @Entity(
     tableName = "note_texts",
+    foreignKeys = [
+        ForeignKey(
+            entity = RoomNoteEntry::class,
+            parentColumns = ["id"],
+            childColumns = ["id"],
+            onDelete = CASCADE,
+            onUpdate = CASCADE,
+        ),
+    ],
 )
 data class RoomNoteText(
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "id")
-    val id: Long,
+    val id: Long = 0L,
 
     @ColumnInfo(name = "text")
     val text: String,
--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/data/NoteDao.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/data/NoteDao.kt
@@ -1,9 +1,10 @@
 package io.github.alexeychurchill.stickynotes.notes.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import androidx.room.Transaction
-import androidx.room.Upsert
 import io.github.alexeychurchill.stickynotes.note_editor.data.RoomNote
 import io.github.alexeychurchill.stickynotes.note_editor.data.RoomNoteText
 import kotlinx.coroutines.flow.Flow
@@ -17,7 +18,7 @@ interface NoteDao {
     @Query("SELECT * FROM note_entries WHERE id=:id LIMIT 1")
     suspend fun getNoteEntry(id: Long): RoomNoteEntry?
 
-    @Upsert(entity = RoomNoteEntry::class)
+    @Insert(entity = RoomNoteEntry::class, onConflict = REPLACE)
     suspend fun upsertNoteEntry(entry: RoomNoteEntry): Long
 
     @Query("DELETE FROM note_entries WHERE id=:id")
@@ -26,8 +27,8 @@ interface NoteDao {
     @Query("SELECT * FROM note_texts WHERE id=:id LIMIT 1")
     suspend fun getNoteText(id: Long): RoomNoteText?
 
-    @Upsert
-    suspend fun upsertNoteText(entity: RoomNoteText): Long
+    @Insert(entity = RoomNoteText::class, onConflict = REPLACE)
+    suspend fun upsertNoteText(entity: RoomNoteText)
 
     @Transaction
     suspend fun getNote(id: Long): RoomNote? {
