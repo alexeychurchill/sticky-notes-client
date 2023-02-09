c29aa05bd0fcfbb5e3b1bfd16201a0d6a6144a4a
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-02-10 01:20:51 +0200

    Implement FK relation

--- app/src/main/java/io/github/alexeychurchill/stickynotes/note_editor/data/RoomNoteRepository.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/note_editor/data/RoomNoteRepository.kt
@@ -4,7 +4,10 @@ import io.github.alexeychurchill.stickynotes.core.DispatcherProvider
 import io.github.alexeychurchill.stickynotes.core.data.Database
 import io.github.alexeychurchill.stickynotes.core.model.Note
 import io.github.alexeychurchill.stickynotes.note_editor.domain.NoteRepository
+import io.github.alexeychurchill.stickynotes.notes.data.toDatabase
+import io.github.alexeychurchill.stickynotes.notes.data.toDomain
 import kotlinx.coroutines.withContext
+import java.util.concurrent.Callable
 import javax.inject.Inject
 
 class RoomNoteRepository @Inject constructor(
@@ -13,15 +16,36 @@ class RoomNoteRepository @Inject constructor(
 ) : NoteRepository {
 
     override suspend fun getNote(id: String): Note? {
+        val noteId = id.toLongOrNull() ?: return null
         return withContext(dispatchers.io) {
-            db.noteEntryDao()
-                .getNote(id = id.toLongOrNull() ?: return@withContext null)
-                ?.toDomain()
+            db.runInTransaction(getNoteTransaction(noteId))
         }
     }
 
     override suspend fun saveNote(note: Note) {
-        val entity = note.toDatabase()
-        db.noteEntryDao().upsertNote(entity)
+        val entryEntity = note.entry.toDatabase()
+        val textEntity = RoomNoteText(
+            id = entryEntity.id,
+            text = note.text,
+        )
+        withContext(dispatchers.io) {
+            db.runInTransaction {
+                db.noteEntryDao().insertNoteEntry(entryEntity)
+                db.noteTextDao().insertNoteText(textEntity)
+            }
+        }
+    }
+
+    private fun getNoteTransaction(noteId: Long): Callable<Note?> = Callable<Note?> {
+        val noteEntry = db.noteEntryDao().getNoteEntry(noteId)
+            ?: return@Callable null
+
+        val text = db.noteTextDao().getNoteText(noteEntry.id)
+            ?.text ?: ""
+
+        return@Callable Note(
+            entry = noteEntry.toDomain(),
+            text = text,
+        )
     }
 }
--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/data/NoteDao.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/data/NoteDao.kt
@@ -4,9 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
-import androidx.room.Transaction
-import io.github.alexeychurchill.stickynotes.note_editor.data.RoomNote
-import io.github.alexeychurchill.stickynotes.note_editor.data.RoomNoteText
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -16,35 +13,11 @@ interface NoteDao {
     fun allNotesFlow(): Flow<List<RoomNoteEntry>>
 
     @Query("SELECT * FROM note_entries WHERE id=:id LIMIT 1")
-    suspend fun getNoteEntry(id: Long): RoomNoteEntry?
+    fun getNoteEntry(id: Long): RoomNoteEntry?
 
     @Insert(entity = RoomNoteEntry::class, onConflict = REPLACE)
-    suspend fun upsertNoteEntry(entry: RoomNoteEntry): Long
+    fun insertNoteEntry(entry: RoomNoteEntry): Long
 
     @Query("DELETE FROM note_entries WHERE id=:id")
-    suspend fun deleteNoteEntry(id: Long)
-
-    @Query("SELECT * FROM note_texts WHERE id=:id LIMIT 1")
-    suspend fun getNoteText(id: Long): RoomNoteText?
-
-    @Insert(entity = RoomNoteText::class, onConflict = REPLACE)
-    suspend fun upsertNoteText(entity: RoomNoteText)
-
-    @Transaction
-    suspend fun getNote(id: Long): RoomNote? {
-        val entryEntity = getNoteEntry(id) ?: return null
-        val text = getNoteText(id)?.text ?: ""
-        return RoomNote(entryEntity, text)
-    }
-
-    @Transaction
-    suspend fun upsertNote(note: RoomNote): Long {
-        val noteEntryId = upsertNoteEntry(note.entry)
-        val noteTextEntity = RoomNoteText(
-            id = noteEntryId,
-            text = note.text,
-        )
-        upsertNoteText(noteTextEntity)
-        return noteEntryId
-    }
+    fun deleteNoteEntry(id: Long)
 }
