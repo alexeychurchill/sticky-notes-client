f3e23e6a734ee2e66c02913eb625f75ea696a35f
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2023-02-10 03:33:57 +0200

    Adjust Note Entry item widget & add Delete UI

--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/ui/NoteEntryListItem.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/ui/NoteEntryListItem.kt
@@ -2,8 +2,14 @@
 package io.github.alexeychurchill.stickynotes.notes.ui
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import io.github.alexeychurchill.stickynotes.R
 import io.github.alexeychurchill.stickynotes.core.model.NoteEntry
 import io.github.alexeychurchill.stickynotes.core.ui.Spacing.Regular
 
@@ -13,12 +19,41 @@ fun NoteEntryListItem(
     onEntryClick: (NoteEntry) -> Unit,
     onEntryDelete: (NoteEntry) -> Unit,
 ) {
-    // TODO: Implement Delete handling
     NoteEntryWidget(
         modifier = Modifier
             .padding(horizontal = Regular)
             .padding(top = Regular),
         entry = noteEntry,
         onClick = { onEntryClick(noteEntry) },
+        endItem = {
+            var isItemMenuExpanded by remember { mutableStateOf(false) }
+            IconButton(onClick = { isItemMenuExpanded = true }) {
+                Icon(
+                    imageVector = Icons.Rounded.MoreVert,
+                    contentDescription = null,
+                )
+            }
+
+            DropdownMenu(
+                expanded = isItemMenuExpanded,
+                onDismissRequest = { isItemMenuExpanded = false },
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        onEntryDelete(noteEntry)
+                        isItemMenuExpanded = false
+                    },
+                    text = {
+                        Text(text = stringResource(R.string.generic_delete))
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Rounded.Delete,
+                            contentDescription = null,
+                        )
+                    },
+                )
+            }
+        },
     )
 }
--- app/src/main/java/io/github/alexeychurchill/stickynotes/notes/ui/NoteEntryWidget.kt
+++ app/src/main/java/io/github/alexeychurchill/stickynotes/notes/ui/NoteEntryWidget.kt
@@ -1,24 +1,22 @@
 package io.github.alexeychurchill.stickynotes.notes.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Alignment.Companion.CenterEnd
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.alexeychurchill.stickynotes.core.datetime.RegularDateTimeFormatter
 import io.github.alexeychurchill.stickynotes.core.model.NoteEntry
+import io.github.alexeychurchill.stickynotes.core.ui.Spacing.Big
 import io.github.alexeychurchill.stickynotes.core.ui.Spacing.Medium
 import io.github.alexeychurchill.stickynotes.core.ui.Spacing.Regular
 import io.github.alexeychurchill.stickynotes.core.ui.StickyNotesTheme
@@ -27,20 +25,18 @@ import java.util.*
 val NoteEntryShape: Shape
     @Composable
     @ReadOnlyComposable
-    get() = MaterialTheme.shapes.large
+    get() = MaterialTheme.shapes.extraLarge
 
 @Composable
 fun NoteEntryWidget(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = { },
+    endItem: @Composable (() -> Unit)? = null,
     entry: NoteEntry,
 ) {
     Box(modifier = modifier) {
         val shape = NoteEntryShape
-        val backgroundColor = MaterialTheme.colorScheme
-            .primary
-            .copy(alpha = 0.3f)
-            .compositeOver(MaterialTheme.colorScheme.surface)
+        val backgroundColor = MaterialTheme.colorScheme.secondaryContainer
 
         Surface(
             modifier = Modifier
@@ -50,38 +46,55 @@ fun NoteEntryWidget(
             color = backgroundColor,
             shape = shape,
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(
-                        horizontal = Medium,
-                        vertical = Regular,
-                    ),
-            ) {
-                Text(
-                    text = entry.title,
-                    style = MaterialTheme.typography.titleSmall,
-                    maxLines = 1,
-                    overflow = Ellipsis,
-                )
-
-                Text(
-                    text = entry.subject ?: "",
-                    style = MaterialTheme.typography.bodyLarge,
-                    maxLines = 2,
-                    overflow = Ellipsis,
-                )
-
-                val date = CurrentDateTimeFormatter
-                    .format(entry.changedAt.timeInMillis)
-
-                Box(modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier
+                        .weight(weight = 1.0f)
+                        .padding(
+                            vertical = Medium,
+                        )
+                        .padding(
+                            start = Big,
+                            end = endItem?.let { Regular } ?: Big,
+                        ),
+                ) {
                     Text(
-                        modifier = Modifier
-                            .padding(top = Medium)
-                            .align(CenterEnd),
-                        text = date,
-                        style = MaterialTheme.typography.bodyMedium,
+                        text = entry.title,
+                        style = MaterialTheme.typography.titleLarge,
+                        maxLines = 1,
+                        overflow = Ellipsis,
                     )
+
+                    Text(
+                        text = entry.subject ?: "",
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 2,
+                        overflow = Ellipsis,
+                    )
+
+                    val date = CurrentDateTimeFormatter
+                        .format(entry.changedAt.timeInMillis)
+
+                    Box(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            modifier = Modifier
+                                .padding(top = Medium)
+                                .align(CenterEnd),
+                            text = date,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
+                }
+
+                endItem?.let { itemBlock ->
+                    Box(
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .align(CenterVertically)
+                            .padding(end = Regular),
+                    ) {
+                        itemBlock()
+                    }
                 }
             }
         }
